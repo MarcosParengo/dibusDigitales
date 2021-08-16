ec985ff04504e5f77b539003b3cb7fcc3f7dd8a6
marcos gaston parengo <marcos.sk8.parengo@gmail.com>
2021-08-16 13:23:56 -0300

    flash

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+
+    document.getElementById('dibu1').addEventListener('click', function(e) {
+        document.getElementById("dibu1").classList.toggle('invertir');
+    });
+
+    document.getElementById('dibu2').addEventListener('click', function(e) {
+        document.getElementById("dibu2").classList.toggle('invertir');
+    });
+};
