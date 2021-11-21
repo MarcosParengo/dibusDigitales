e93416579ef20009799bcb49716e322c0e85c660
MarcosParengo <marcos.sk8.parengo@gmail.com>
2021-11-20 21:14:10 -0300

    3er dibujo y flashes

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-window.onload = function() {
-
-    document.getElementById('dibu1').addEventListener('click', function(e) {
-        document.getElementById("dibu1").classList.toggle('invertir');
-    });
-
-    document.getElementById('dibu2').addEventListener('click', function(e) {
-        document.getElementById("dibu2").classList.toggle('invertir');
-    });
-};
+function invertir(id) {
+    document.getElementById(id).classList.toggle('invertir');
+}
