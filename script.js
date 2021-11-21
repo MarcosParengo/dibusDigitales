7a68fd829867aaca379055ad36e4e6efae39220a
MarcosParengo <marcos.sk8.parengo@gmail.com>
2021-11-21 01:35:09 -0300

    Version 0.1

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function invertir(id) {
-    document.getElementById(id).classList.toggle('invertir');
+function invertirYRotar(id) {
+	document.getElementById(id).classList.remove('invertir');
+	document.getElementById(id).classList.toggle('invertirYCambiarColor');
+	document.getElementById(id).classList.toggle('rotar3d');
 }
+
+function invertir(id) {
+	document.getElementById(id).classList.toggle('invertir');
+}
+
+window.onload = function() {
+    $('#cargando').addClass("oculto")
+	var myModal = document.getElementById('myModal')
+    var noticed = localStorage.getItem('noticed');
+	if (noticed === null) {
+		$('#myModal').modal('show');
+	}
+	myModal.addEventListener('shown.bs.modal', function() {
+		localStorage.setItem('noticed',true)
+	});
+};
