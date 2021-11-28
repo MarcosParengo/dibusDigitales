8c2f297d7c6ec47ad517802905f1b1add3d127f0
MarcosParengo <marcos.sk8.parengo@gmail.com>
2021-11-28 15:33:43 -0300

    Prueba google

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
 function invertirYRotar(id) {
 	document.getElementById(id).classList.remove('invertir');
 	document.getElementById(id).classList.toggle('invertirYCambiarColor');
@@ -9,18 +13,26 @@ function invertir(id) {
 }
 
 window.onload = function() {
+	// Initialize Cloud Firestore through Firebase
+	firebase.initializeApp({
+		apiKey: '### FIREBASE API KEY ###',
+		authDomain: '### FIREBASE AUTH DOMAIN ###',
+		projectId: '### CLOUD FIRESTORE PROJECT ID ###'
+	});
 
-    $('#body').addClass("loaded")
-	$('#cargando').addClass("oculto")
-	var myModal = document.getElementById('myModal')
-    var noticed = localStorage.getItem('noticed');
+	var db = firebase.firestore();
+	
+	$('#body').addClass('loaded');
+	$('#cargando').addClass('oculto');
+	var myModal = document.getElementById('myModal');
+	var noticed = localStorage.getItem('noticed');
 	if (noticed === null) {
 		$('#myModal').modal('show');
 	}
 	myModal.addEventListener('shown.bs.modal', function() {
-		localStorage.setItem('noticed',true)
+		localStorage.setItem('noticed', true);
 	});
-	$('#image1').zoom({ on: 'click',duration:'1000' });
-	$('#image2').zoom({ on: 'click',duration:'500' });
-	$('#image3').zoom({ on: 'click',magnify: 1.3 });
+	$('#image1').zoom({ on: 'click', duration: '1000' });
+	$('#image2').zoom({ on: 'click', duration: '500' });
+	$('#image3').zoom({ on: 'click', magnify: 1.3 });
 };
